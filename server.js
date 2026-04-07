bdae0549e5da11346c83e953bc2110e6cf7b2182
Haritha <tharaharitha196@gmail.com>
2026-04-07 06:26:27 +0530

    fix port for deployment

--- server.js
+++ server.js
@@ -58,4 +58,5 @@ app.post('/login', (req, res) => {
     });
 });
 // ------------------------------
-app.listen(5000, () => console.log('Server live on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server live on port ${PORT}`));
